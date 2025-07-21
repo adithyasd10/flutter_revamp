b165a66ae9299b390ad3f21c56a46c0f584ddb9f
adithyasd10 <adithyasd@gmail.com>
2025-07-21 23:21:22 +0530

    created a minimal profile section

--- lib/main.dart
+++ lib/main.dart
@@ -13,51 +13,62 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[400],
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        toolbarHeight: 70,
         backgroundColor: Colors.grey[900],
+        centerTitle: true,
         title: Text(
-          style: TextStyle(
-            letterSpacing: 2,
-            color: Colors.grey[300],
+          style: TextStyle(color: Colors.grey[350],letterSpacing: 4.0),
+          'PROFILE')
+      ),
+      body: Padding(
+        padding: EdgeInsetsGeometry.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  backgroundImage: AssetImage('assets/zoro.jpg'),
+                  radius: 80.0,
+                
+                ),
+              ],
+            ),
+            SizedBox(height: 10.0),
+            Divider(height: 40.0, color: Colors.grey[600]),
+            Text(
+              style: TextStyle(color:Colors.grey[400],letterSpacing:4.0),
+              'NAME'),
+              SizedBox(height: 12.0),
+            Text(
+              style: TextStyle(color:Colors.white60,letterSpacing:4.0,fontSize: 40,fontWeight:FontWeight.bold),
+              'ZORO'),
+              SizedBox(height: 10.0),
+            Text(
+              style: TextStyle(color:Colors.grey[400],letterSpacing:4.0),
+              'CURRENT POSITION'),
+              SizedBox(height: 12.0),
+            Text(
+              style: TextStyle(color:Colors.white60,letterSpacing:4.0,fontSize: 40,fontWeight:FontWeight.bold),
+              'Luffys Right Hand Man'),
+            SizedBox(height: 12.0),
+            Row(
+              children: [
+                Icon(Icons.email,color: Colors.grey[400]),
+                SizedBox(width: 10.0),
+            Text(
+              style: TextStyle(color:Colors.grey[400],letterSpacing:2.0),
+              'zoro@gmail.com'),
+              ],
+            ),
             
-          ),
-          'First App'),
-          centerTitle: true,
-      ),
-      body:Column(
-        mainAxisAlignment : MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Container(
-            color: Colors.green[50],
-            padding: EdgeInsets.all(30.0),
-            child:Text('one')
-          ),
-          Container(
-            color: Colors.green[100],
-            padding: EdgeInsets.all(30.0),
-            child:Text('two')
-          ),
-          Container(
-            color: Colors.green[200],
-            padding: EdgeInsets.all(30.0),
-            child:Text('three')
-          )
-        ]
-      ),
-      
 
-      floatingActionButton: FloatingActionButton(
-        onPressed:() {
-        },
-        backgroundColor: Colors.grey[900],
-        child:Text(
-          style: TextStyle(
-            color: Colors.grey[300],
-          ) ,
-          'hit me')
+            
+            
+          ],
+        ),
       ),
     );
   }
